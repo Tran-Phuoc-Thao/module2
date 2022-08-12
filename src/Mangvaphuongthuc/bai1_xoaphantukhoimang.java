9efe78d0fad859291fa0c946e827d1c931575c6a
Tran-Phuoc-Thao <tranphuocthao92@gmail.com>
2022-08-12 19:08:30 +0700

    xong vong lap

---
+++ src/Mangvaphuongthuc/bai1_xoaphantukhoimang.java
@@ -0,0 +1,58 @@
+package Mangvaphuongthuc;
+
+import java.util.Scanner;
+
+public class bai1_xoaphantukhoimang {
+    public static int Index(int x, int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (x == array[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int size, so;
+
+        Scanner Scanner = new Scanner(System.in);
+        System.out.println(" Nhập kích thước của mảng < 20 ");
+
+        do {
+            size = Scanner.nextInt();
+        } while (size > 20);
+
+        int array[] = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print(" Nhập phần tử thứ " + (i + 1));
+            array[i] = Scanner.nextInt();
+        }
+
+        System.out.println("Nhập phần tử cần xóa");
+        so = Scanner.nextInt();
+
+        if (Index(so, array) == -1) {
+            System.out.print("So can xoa khong co trong mang");
+
+        } else {
+            int index = Index(so, array);
+            System.out.println("mảng trước khi xóa là ");
+            for (int i : array) {
+                System.out.print(i + "\t");
+            }
+            for (int i = index; i < array.length - 1; i++) {
+                array[i] = array[i + 1];
+            }
+            array[array.length - 1] = 0;
+            System.out.println("mảng sau khi xóa là ");
+            for (int i : array) {
+                System.out.print(i + "\t");
+            }
+        }
+
+    }
+
+
+}
---
+++ src/Mangvaphuongthuc/bai2_themphantuvaomang.java
@@ -0,0 +1,49 @@
+package Mangvaphuongthuc;
+
+import java.util.Scanner;
+
+public class bai2_themphantuvaomang {
+    public static void sx(int x,int array[]){
+        for (int i = x ; i < array.length-1; i ++){
+
+            array[i+1]=array[i];
+
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        int size, so, index;
+
+        Scanner Scanner = new Scanner(System.in);
+        System.out.println(" Nhập kích thước của mảng ");
+        size = Scanner.nextInt();
+
+        int array[] = new int[size+1];
+
+        for (int i = 0; i < size ; i++) {
+            System.out.print(" Nhập phần tử thứ " + (i + 1) + " : ");
+            array[i] = Scanner.nextInt();
+
+        }
+
+
+        System.out.println("nhập số cần chèn : ");
+        so = Scanner.nextInt();
+        System.out.println("nhập vị trí cần chèn : ");
+        index = Scanner.nextInt() - 1;
+        sx(index,array);
+        array[index]=so;
+
+        System.out.println("mảng sau khi chèn là : ");
+        for (int i : array) {
+            System.out.print(i + "\t");
+        }
+
+
+
+
+    }
+
+}
