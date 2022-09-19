87e68240c18961edbefa9431867fd7b228be309b
Tran-Phuoc-Thao <tranphuocthao92@gmail.com>
2022-09-19 19:19:00 +0700

    update

---
+++ src/Stack_Queue/chuyendoi.java
@@ -0,0 +1,23 @@
+package Stack_Queue;
+import java.util.Scanner;
+import java.util.Stack;
+public class chuyendoi {
+    public static void convert(int number) {
+        Stack<Integer> stack = new Stack<>();
+        while (number > 0) {
+            int temp = number % 2;
+            stack.push(temp);
+            number /= 2;
+        }
+        final int SIZE= stack.size();
+        for(int i=0; i<SIZE;i++){
+            System.out.print(stack.pop()+" ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Nhap so can chuyen doi: ");
+        int number= sc.nextInt();
+        convert(number);
+    }
+}
